0753b760b568322465a6243e7675a1a7581f7aa1
goknurderinoz <goknur.derinoz@gmail.com>
2023-03-31 08:54:13 +0300

    (+) Doxygen comment

---
+++ docs/html/search/functions_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['readdatafromtcpsocket_0',['readDataFromTCPSocket',['../class_main_window.html#aaeb2e5b70c7ac387357cb59cfc4ce44b',1,'MainWindow']]]
+];
---
+++ docs/html/search/variables_1.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['m_5fclients_0',['m_clients',['../class_main_window.html#adb2eacd3f1390d07ea37fa7a7a9bf2a3',1,'MainWindow']]],
+  ['m_5fdebug_1',['m_debug',['../class_main_window.html#a1118d5d0f5b4b472f10c75d18786b1f1',1,'MainWindow']]],
+  ['m_5fpwebsocketserver_2',['m_pWebSocketServer',['../class_main_window.html#add862af236395d783d7e3661a920ac1e',1,'MainWindow']]],
+  ['manager_3',['manager',['../class_main_window.html#ada57966f51638291da96f299bf0cf267',1,'MainWindow']]]
+];
